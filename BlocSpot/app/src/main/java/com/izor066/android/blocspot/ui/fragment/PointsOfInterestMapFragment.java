093edf28c5d66977db8f5732b02598a401bfdaf3
i-zor066 <igor.zor@gmail.com>
2015-09-28 16:02:12 +0100

    Added initial map fragment (PointsOfInterestMapFragment).

--- BlocSpot/app/src/main/java/com/izor066/android/blocspot/ui/fragment/PointsOfInterestMapFragment.java
+++ BlocSpot/app/src/main/java/com/izor066/android/blocspot/ui/fragment/PointsOfInterestMapFragment.java
@@ -1,13 +1,20 @@
 package com.izor066.android.blocspot.ui.fragment;
 
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.izor066.android.blocspot.R;
 
 /**
@@ -17,11 +24,68 @@ import com.izor066.android.blocspot.R;
 public class PointsOfInterestMapFragment extends Fragment {
 
 
+
+    MapView mapView;
+    private GoogleMap googleMap;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        
         View view = inflater.inflate(R.layout.fragment_points_of_interest_map, container, false);
+        mapView = (MapView) view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.onResume();
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mapView.getMap();
+
+        double latitude = 51.5160563;
+        double longitude = -0.1271485;
+
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("Google HQ London");
+
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 }
