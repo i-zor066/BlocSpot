26aa33dd157fda5ce7ac56cd01329e8fb5d46761
i-zor066 <igor.zor@gmail.com>
2015-09-28 02:21:34 +0100

    Since the use of ActionBar was deprecated, Swipeable tab like functionality was implemented with the help SlidingTabLayout and SlidingTabLayout strip (https://github.com/google/iosched/blob/master/android/src/main/java/com/google/samples/apps/iosched/ui/widget/SlidingTabLayout.java and https://github.com/google/iosched/blob/master/android/src/main/java/com/google/samples/apps/iosched/ui/widget/SlidingTabStrip.java)

---
+++ BlocSpot/app/src/main/java/com/izor066/android/blocspot/ui/fragment/PointsOfInterestMapFragment.java
@@ -0,0 +1,27 @@
+package com.izor066.android.blocspot.ui.fragment;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.izor066.android.blocspot.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+
+public class PointsOfInterestMapFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_points_of_interest_map, container, false);
+        return view;
+    }
+
+
+}
